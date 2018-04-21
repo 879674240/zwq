5a5da13aa0c4c1cb0536042caef27287fed16bf6
zwq <879674240@qq.com>
2018-04-21 18:49:03 +0800

    框架搭建完成

---
+++ IPMS/common/src/main/java/com/xupt/common/BizException.java
@@ -0,0 +1,29 @@
+package com.xupt.common;
+
+public class BizException extends Exception {
+
+    //无参构造方法
+    public BizException(){
+
+        super();
+    }
+
+    //有参的构造方法
+    public BizException(String message){
+        super(message);
+
+    }
+
+    // 用指定的详细信息和原因构造一个新的异常
+    public BizException(String message, Throwable cause){
+
+        super(message,cause);
+    }
+
+    //用指定原因构造一个新的异常
+    public BizException(Throwable cause) {
+
+        super(cause);
+    }
+
+}
---
+++ IPMS/common/src/main/java/com/xupt/common/PageResult.java
@@ -0,0 +1,44 @@
+package com.xupt.common;
+
+/**
+ * 分页参数
+ * @param <T>
+ */
+public class PageResult<T> {
+    private int currentPage;
+    private int pageSize;
+    private T data;
+    private int count;
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
